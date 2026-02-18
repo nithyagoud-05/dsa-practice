76421af13e7a271e4700ad8e52b5cf1766c6db33
nithyagoud-05 <nithyagoudelikatti@gmail.com>
2026-02-18 08:42:34 +0530

    Updated Optimal logic

--- Arrays.cpp/Sort_0s_1s_2s.cpp
+++ Arrays.cpp/Sort_0s_1s_2s.cpp
@@ -1,27 +1,22 @@
-
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size(),c1=0,c0=0,c2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==0) c0++;
-            else if(nums[i]==1) c1++;
-            else c2++;
+        int n=nums.size(),low=0,mid=0;
+        int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
-        for(int i=0;i<c0;i++){
-            nums[i]=0;
-        }
-         for(int i=c0;i<c0+c1;i++){
-            nums[i]=1;
-        }
-         for(int i=c0+c1;i<n;i++){
-            nums[i]=2;
-        }
-        for(int x: nums){
-            cout<<x<<" "<<endl;
-        }
-
-        
+ 
     }
 };
-
